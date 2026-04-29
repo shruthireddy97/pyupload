8f5b297b3216c37faa3a1a623c15f75ea38e556b
shruthireddy97 <59198608+shruthireddy97@users.noreply.github.com>
2026-04-29 00:18:54 -0400

    Delete be directory

--- be/routers/record.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, UploadFile, File
-from services.transcriber import transcribe_audio
-
-router = APIRouter(prefix="/record", tags=["Record"])
-
-@router.post("/")
-async def record_audio(file: UploadFile = File(...)):
-    transcript = transcribe_audio(file.file)
-    return {"transcript": transcript}
